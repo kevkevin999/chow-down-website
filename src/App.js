187dc666eab675b44674ff729e87cfff2ab6b5da
nickhitos <nickhit3802@gmail.com>
2022-12-21 13:25:30 -0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from "./Home"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import { Routes, Route, Link } from "react-router-dom";
 
+// Routes to each page
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
