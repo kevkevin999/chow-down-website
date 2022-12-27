a4b3f5c9356f6e6fa0cc7e05ad374a6cfd8e4a99
nickhitos <nickhit3802@gmail.com>
2022-12-27 12:31:52 -0800

    implemented map using Leaflet with no styling

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Map from "./components/Map";
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Map />
             <Navbar />
         </div>
     );
